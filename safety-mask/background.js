6445a7aa13a66dd1f24f44d1e22a126189b91864
Your Name <you@example.com>
2020-04-14 14:54:48 -0400

    new

---
+++ safety-mask/background.js
@@ -0,0 +1,19 @@
+chrome.alarms.onAlarm.addListener(function() {
+  chrome.browserAction.setBadgeText({text: ''});
+  chrome.notifications.create({
+      type:     'basic',
+      iconUrl:  'stay_hydrated.png',
+      title:    'Time to Hydrate',
+      message:  'Everyday I\'m Guzzlin\'!',
+      buttons: [
+        {title: 'Keep it Flowing.'}
+      ],
+      priority: 0});
+});
+
+chrome.notifications.onButtonClicked.addListener(function() {
+  chrome.storage.sync.get(['minutes'], function(item) {
+    chrome.browserAction.setBadgeText({text: 'ON'});
+    chrome.alarms.create({delayInMinutes: item.minutes});
+  });
+});
---
+++ safety-mask/popup.js
@@ -0,0 +1,34 @@
+
+
+function setAlarm(event) {
+  let minutes = parseFloat(event.target.value);
+  chrome.browserAction.setBadgeText({text: 'ON'});
+  chrome.alarms.create({delayInMinutes: minutes});
+  chrome.storage.sync.set({minutes: minutes});
+  window.close();
+}
+
+function clearAlarm() {
+  chrome.browserAction.setBadgeText({text: ''});
+  chrome.alarms.clearAll();
+  window.close();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var links = document.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        (function () {
+            var ln = links[i];
+            var location = ln.href;
+            ln.onclick = function () {
+                chrome.tabs.create({active: true, url: location});
+            };
+        })();
+    }
+})
+
+
+document.getElementById('sampleSecond').addEventListener('click', setAlarm);
+document.getElementById('1min').addEventListener('click', setAlarm);
+document.getElementById('30min').addEventListener('click', setAlarm);
+document.getElementById('cancelAlarm').addEventListener('click', clearAlarm);
